772f03a4197524339b6dc49860a47e6126ef0e09
Torin-Perkins <tdmp42@gmail.com>
2017-05-15 20:31:57 -0700

    working on sign in

--- app/src/main/java/com/java/torin/listnermodule/MainActivity.java
+++ app/src/main/java/com/java/torin/listnermodule/MainActivity.java
@@ -1,6 +1,7 @@
 package com.java.torin.listnermodule;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +9,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-
+    Intent myIntent;
     ListenerHelper Listener ;
     Vibrator v ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myIntent = new Intent(this, NameActivity.class);
        /* Listener = new ListenerHelper();
         v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         Listener.runListener();
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         }
 */
 
+        startActivity(myIntent);
 
 
     }
--- app/src/main/java/com/java/torin/listnermodule/NameActivity.java
+++ app/src/main/java/com/java/torin/listnermodule/NameActivity.java
@@ -3,6 +3,7 @@ package com.java.torin.listnermodule;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by Admin on 5/13/2017.
@@ -10,12 +11,29 @@ import android.support.v7.app.AppCompatActivity;
 
 public class NameActivity extends AppCompatActivity {
     SignInHelper s = new SignInHelper();
+    EditText firstName, lastName;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.name_activity);
+        s.firstName = getFirstName();
+
 
 
     }
+    public String getFirstName(){
+        String newString = firstName.getText().toString();
+        firstName = (EditText) findViewById(R.id.FirstName);
+
+        while(newString.equals("First Name") ){
+            if(!newString.equals("FirstName") || newString.isEmpty()){
+                return newString;
+            }
+
+        }
+        return null;
+    }
+
+
 
 
 
